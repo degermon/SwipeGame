194559c6ad36444f1a3dfe165822e0ea22e7793c
Daniel Suskevic <dsuskevic@klt-applemacmookpro1.local>
2017-11-11 16:32:51 +0200

    A swipe game, not finished, does not have game over and win parts, when animation is in motion does not recognize swipes

--- SwipeGame/ViewController.swift
+++ SwipeGame/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    var number : Int = 0
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label1.text = String(number)
+        timerExample()
+        animate1()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func label1SwipeRight(_ sender: UISwipeGestureRecognizer) {
+        number += 1
+        label1.text = String(number)
+    }
+    
+    func animate1() {
+        UIView.animate(withDuration: 4.0, animations: {
+            let num1 : CGFloat = CGFloat(arc4random_uniform(667))
+            let num2 : CGFloat = CGFloat(arc4random_uniform(375))
+            self.label1.frame.origin.y = num1
+            self.label1.frame.origin.x = num2
+        }, completion: {(bool) in
+            self.animate1()
+            print("Animation1 completed")
+        })
+    }
+    
+    func timerExample() {
+        Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        label1.text = String(Int(label1.text!)! + 1)
+    }
+    
 }
 
